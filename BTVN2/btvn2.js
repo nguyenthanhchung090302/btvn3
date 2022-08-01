6964d49e9ecfe4776bf2ede2860259a6489c821b
nguyenthanhchung090302 <110025385+nguyenthanhchung090302@users.noreply.github.com>
2022-08-02 00:21:05 +0700

    Add files via upload

---
+++ BTVN2/btvn2.js
@@ -0,0 +1,70 @@
+// bài 1  Sử dụng vòng lặp để đếm từ 1 đến 100. 
+//Khi số là 99, hiển thị hộp thoại thông báo là đã hoàn thành
+
+// for(let i = 0; i < 100; i=i+1){
+//      console.log(i)
+// }console.log(99,"đã hoàn thành");
+
+// bài 2
+//  let c = 0, d = 1, nextTerm;
+//  console.log('Fibonacci Series:');
+// for (let i = 1 ; i<= 20 ; i=i + 1){
+//     console.log(c);
+//     nextTerm = c + d;
+//      c = d;
+//      d = nextTerm;
+//  }
+ // bài 3
+
+
+//  let a = 0 , b = 1 ,nextTerm;
+//  console.log('Fibonnacci Series:');
+//  for (let i = 1 ; i <=3; i = i + 1){
+//      nextTerm = a + b;
+//      a = b;
+//      b = nextTerm;
+//  }console.log(a + b )%5;
+
+
+ // bài 4
+
+
+//  let a = 0 , b = 1 ,nextTerm;
+//  console.log ('Fibonnacci Series:');
+//  for (let i = 1 ; i<= 20; i = i+ 1 ){
+//      nextTerm = a + b;
+//      a = b
+//      b = nextTerm;
+
+//  }console.log(a + b + nextTerm);
+
+// bài 6
+
+//     for ( i = 0; i <= 100 ; i = i+ 1){
+//              if(  i % 3 === 0){
+//              console.log(`${i}- Fizz`);
+//           }else if( i %5 ===0) {
+//              console.log(`${i}-Buzz`);
+//           }else if(i %3 ===0 && i % 5 ===0) {
+     
+//              console.log(`${i}- FizzBuzz`);
+//           }
+     
+//          }
+  // bài 5
+///                  em chịu thua rồi. em chúc thầy ngủ ngon
+// let a = 30;
+// let b = 0;
+// if(a > 0 && Number.isInteger(a)){
+// for(let i = 1; i <= a; i = i + 1){
+//  let b = i / 7
+//  console.log(b);
+// }
+// }
+
+
+
+
+
+
+
